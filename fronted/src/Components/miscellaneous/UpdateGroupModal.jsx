b91c48a8aca3b5b2115e5c048eb231a09fcdc4c5
Uma Sahni <monkeytown64@gmail.com>
2024-07-06 15:59:20 +0530

    Group adding and removing is working

--- fronted/src/Components/miscellaneous/UpdateGroupModal.jsx
+++ fronted/src/Components/miscellaneous/UpdateGroupModal.jsx
@@ -1,5 +1,5 @@
 import { ViewIcon } from '@chakra-ui/icons'
-import { Box, Button, Center, FormControl, IconButton, Input, useDisclosure, useToast } from '@chakra-ui/react'
+import { Box, Button, Center, FormControl, IconButton, Input, Spinner, useDisclosure, useToast } from '@chakra-ui/react'
 import React, { useContext, useState } from 'react'
 import {
     Modal,
@@ -14,6 +14,7 @@ import { ChatContext } from '../../Context/ChatProvider'
 import UserBadgeItem from './UserBadgeItem'
 import { AuthContext } from '../../Context/AuthContext'
 import axios from 'axios'
+import UserListItem from './UserListItem'
 
 const UpdateGroupModal = ({ fetchAgain, setFetchAgain }) => {
     const {
@@ -33,9 +34,7 @@ const UpdateGroupModal = ({ fetchAgain, setFetchAgain }) => {
     const {token} = useContext(AuthContext)
     const toast = useToast()
 
-    const handleRemove =(user)=>{
 
-    }
 
     const handleRename = async()=>{
         if(!groupChatName) return
@@ -81,11 +80,144 @@ const UpdateGroupModal = ({ fetchAgain, setFetchAgain }) => {
         }
     }
 
-    const handleSearch =()=>{
+    const handleSearch = async (e) => {
+        const searchValue = e.target.value
+        if (!searchValue) {
+            toast({
+                position: "top-left",
+                title: "Please Enter Something in search",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+            });
+            return;
+        }
+
+        try {
+            setLoading(true);
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            };
+            const { data } = await axios.get(
+                `http://localhost:8080/user?search=${searchValue}`,
+                config
+            );
+            // console.log(data);
+            setSearchResult(data);
+            // if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats])
+
+
+            setLoading(false);
+        } catch (error) {
+            console.log(error)
+            setLoading(false);
+            toast({
+                position: "top-left",
+                title: "Error Occured",
+                description: "Failed to load the search results",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+            });
+        }
+    };
+
+    const handleAddUser =async(user1)=>{
+    // If in the list of selected user the user is already there
+    const checkUserPresent = selectedChat.users.find((u)=>u._id == user1._id)
+    if(checkUserPresent) {
+        toast({
+            position: "top-left",
+            title: "User is already in group",
+            status: "warning",
+            duration: 5000,
+            isClosable: true,
+        });
+    }
+
+    // If the user is admin or not
+    if(selectedChat.groupAdmin._id !== user._id){
+        toast({
+            position: "top-left",
+            title: "Only admin can add someone!",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+        });
+    }
+
+    try {
+        setLoading(true)
+        const config = {
+            headers:{
+                Authorization : `Bearer ${token}`
+            }
+        }
+        const { data } = await axios.put(`http://localhost:8080/chat/addtogroup`,{
+            chatId:selectedChat._id,
+            userId:user1._id
+        },config)
+        setSelectedChat(data)
+        setFetchAgain(!fetchAgain)
+        setLoading(false)
+    } catch (error) {
+        toast({
+            position: "top-left",
+            title: "Error Occured",
+            description: error.message.data.message,
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+
+        });
+    }
 
     }
 
+    const handleRemove = async(user1) => {
+        if(selectedChat.groupAdmin._id !== user._id && user1._id !== user._id){
+            toast({
+                position: "top-left",
+                title: "Only admin can remove someone!",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+            })
+            return
+        }
+        try {
+            
+    
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+        const { data } = await axios.put(`http://localhost:8080/chat/groupremove`, {
+            chatId: selectedChat._id,
+            userId: user1._id
+        }, config)
 
+        // if the user has removed himself
+        user1._id == user._id ? setSelectedChat() : setSelectedChat(data)
+        
+        setFetchAgain(!fetchAgain)
+        setLoading(false)
+        } catch (error) {
+            toast({
+                position: "top-left",
+                title: "Error Occured",
+                description: error.message.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+
+            });
+        }
+
+    }
 
 
   return (
@@ -138,9 +270,27 @@ const UpdateGroupModal = ({ fetchAgain, setFetchAgain }) => {
                             <Input
                             placeholder='Add User to group'
                             mb={1}
-                            onChange={(e)=>handleSearch(e.target.value)}
+                            onChange={(e)=>handleSearch(e)}
                             />
                         </FormControl>
+
+                        {/* User list Item */}
+                        {
+                            loading ? (
+                                <Spinner size={"lg"} />
+                            ):
+                            (
+                                searchResult?.map((user)=>(
+                                    <UserListItem
+                                    key={user._id}
+                                    name={user.name}
+                                    email={user.email}
+                                    pic={user.pic}
+                                    handleFunction={()=>handleAddUser(user)}
+                                    />
+                                ))
+                            )
+                        }
                   </ModalBody>
 
                   <ModalFooter>
